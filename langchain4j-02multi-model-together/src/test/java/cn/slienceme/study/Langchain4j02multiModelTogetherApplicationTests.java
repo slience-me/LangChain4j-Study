00c8051e5f1726845fd7425325d17bea0d0942ab
slience_me <slienceme.cn@gmail.com>
2025-07-07 18:33:54 +0800

    add 02&03-boot-intergration

---
+++ langchain4j-02multi-model-together/src/test/java/cn/slienceme/study/Langchain4j02multiModelTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.slienceme.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4j02multiModelTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
